32945fb424d0421af0df579284d731ce07fc8ac2
Niklas Mähler <niklas.mahler@gmail.com>
2025-07-03 19:01:24 +0200

    Update tailwindcss

--- tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./http/templates/*.tmpl", "./http/static/input.css"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
